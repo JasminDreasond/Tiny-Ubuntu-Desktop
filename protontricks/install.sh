550611d10569a9383da5f93b6d5917eae01d0953
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-02-05 15:11:20 -0300

    protontricks

---
+++ protontricks/install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install python3-pip python3-setuptools python3-virtualenv
+sudo apt install python3.10-venv
+
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+
+pipx install protontricks
