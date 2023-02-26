35913131acb6e137ccfd25b746ffa9b1f7b2c581
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-02-25 23:19:43 -0300

    hello

--- tts/install.sh
+++ tts/install.sh
@@ -5,3 +5,11 @@ sudo apt-get install festival -y
 sudo apt-get install speech-dispatcher -y
 
 sudo apt-get install espeak -y
+
+say "hello"
+
+echo "hello" | festival --tts
+
+spd-say "hello"
+
+espeak "hello"
