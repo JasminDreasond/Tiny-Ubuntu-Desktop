865a0f75606bcaede38425db37b592b6e8342c2e
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-23 12:37:47 -0300

    libffmpeg so

---
+++ opera/libffmpeg.so.sh
@@ -0,0 +1,10 @@
+read -p "What is the chromium-codecs-ffmpeg-extra file name? " deb_libffmpeg_file 
+
+sudo mkdir chromium-browser
+
+ar x $deb_libffmpeg_file libffmpeg.tar.xz
+
+tar -xJf libffmpeg.tar.xz --strip-components=4 ./usr/lib/chromium-browser/libffmpeg.so
+
+sudo rm $deb_libffmpeg_file
+sudo rm libffmpeg.tar.xz
